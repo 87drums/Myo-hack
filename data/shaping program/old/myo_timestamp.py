afc98681c21941b49325542b43a2c45521478bfb
87drums <g2117048@gmail.com>
2018-06-12 17:40:36 +0900

    unification

--- data/shaping program/old/myo_timestamp.py
+++ data/shaping program/old/myo_timestamp.py
@@ -1,6 +1,11 @@
 #-*- coding:utf-8-*-
 import datetime as dt
 
+"""
+スタックしたデータを一気に受信しているのか，同じタイムスタンプが重なる現象があるので，
+刻み幅を計算し，最初のタイムスタンプに刻み幅を加算していく形式でタイムスタンプを再設定
+"""
+
 #GMT（イギリス）の時間に合わせたタイムスタンプにするため，9時間分-してタイムスタンプを押す
 if __name__ == "__main__":
 #整形対象ファイル読み込み
@@ -19,14 +24,28 @@ if __name__ == "__main__":
     writecontents = "$interval 5\n$var time msec\n$var emg1 short\n$var emg2 short\n$var emg3 short\n$var emg4 short\n$var emg5 short\n$var emg6 short\n$var emg7 short\n$var emg8 short\n\n$data\n" #header
     wfn.write(writecontents)
 
-    #内容書き出し
+    """
+    #サンプリングレート計算→タイムスタンプの刻み幅計算
     ftimestamp = int(segcontent[1].split(",")[0]) / 1000 #初期時間 first timestamp　msへ変換
     ltimestamp = int(segcontent[len(segcontent) - 2].split(",")[0]) / 1000 #最終時間 last timestamp　msへ変換 最後に空文字入ってるため-2
     sr = 1000.0 / ((ltimestamp - ftimestamp) / (len(segcontent) - 3)) #sampling rate
     time_stride = float("%3.4f" % (1000.0 / sr)) #刻み幅，ms単位 ファイルの最初と最後からサンプリングレート計算，刻み幅を厳密に設定→誤差少なくなる？
+    """
 
-    print(time_stride)
+    #最初と最後のタイムスタンプ取得→データ数で割って刻み幅計算
+    ftimestamp = int(segcontent[1].split(",")[0]) / 1000 #初期時間 first timestamp　msへ変換
+    #最後のタイムスタンプで，重なっている部分は除外し計算
+    ltimestamp = int(segcontent[len(segcontent) - 2].split(",")[0]) / 1000 #最終時間 last timestamp　msへ変換 最後に空文字入ってるため-2
+    for i, row in enumerate(reversed(segcontent[:-1])):
+        if (int(row.split(",")[0]) / 1000) != ltimestamp:
+            calc_size = i
+            break
 
+    time_stride = float("%3.4f" % ((ltimestamp - ftimestamp) / float(len(segcontent) - 3 - calc_size))) #刻み幅，ms単位 ファイルの最初と最後からサンプリングレート計算，刻み幅を厳密に設定→誤差少なくなる？
+
+    print(time_stride)#刻み幅確認（ms）
+
+    #内容書き出し
     for i, row in enumerate(segcontent[1:]):
         if row.split(",")[0] is "":
             break
@@ -40,7 +59,7 @@ if __name__ == "__main__":
         #またがない場合
         else:
             x_sum = (((x.hour - 9) * 60 * 60) + (x.minute * 60) + (x.second)) * 1000 + int(x.microsecond / 1000)
-            """
+        """
         x_sum = ((x.hour * 60 * 60) + (x.minute * 60) + (x.second)) * 1000 + int(x.microsecond / 1000)
         #writecontent = str(int(ftimestamp + (time_stride * i))) # + (9 * 60 * 60 * 1000)) #初期時間に時間スライド分と9時間分（ms）足す
         writecontent = str(x_sum)
